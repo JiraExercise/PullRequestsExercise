d1c02ea6c49ed6b16e0554af3f249969fd556569
Martin Poulsen <mapo0004@stud.kea.dk>
2024-09-11 10:24:24 +0200

    k
    k

---
+++ src/main/java/stud/kea/dk/pullrequestsexercise/PullRequestsExerciseApplication.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.pullrequestsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestsExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestsExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stud/kea/dk/pullrequestsexercise/PullRequestsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.pullrequestsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
